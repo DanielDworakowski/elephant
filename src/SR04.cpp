ce958e0c8613a87402abc6061e9ab14f38a07129
Fan Zhang <fanwashere@gmail.com>
2017-03-16 19:53:05 -0400

    Update SR04 interface.

--- src/SR04.cpp
+++ src/SR04.cpp
@@ -20,11 +20,16 @@
  * License:
  *  Public Domain
  */
- #include "SR04.hpp"
+
+#include "SR04.hpp"
+
+#define DEBUG true
+#define dprint(x) do { if (DEBUG) Serial.print(x); } while (0)
+#define dprintln(x) do { if (DEBUG) Serial.println(x); } while (0)
 
 // Anything over 400 cm (23200 us pulse) is "out of range"
 const unsigned int MAX_DIST = 23200;
-const float CONVERSION_CONSTANT_CM = 1 / 58.0;
+const float CONVERSION_CONSTANT_CM = 1 / 58.2;
 const float CONVERSION_CONSTANT_INCH = 1 / 148.0;
 
 SR04::SR04(int trig_pin, int echo_pin)
@@ -32,39 +37,33 @@ SR04::SR04(int trig_pin, int echo_pin)
     _trig_pin = trig_pin;
     _echo_pin = echo_pin;
 
-    // Initialize trigger pin.
     pinMode(_trig_pin, OUTPUT);
-    digitalWrite(_trig_pin, LOW);
+    pinMode(_echo_pin, INPUT);
+
+    dprintln("//// Hardware - SR04 Ultrasonic Sensor initialized.");
 }
 
 void SR04::distanceMeasure(void) 
 {
-    unsigned long t1;
-    unsigned long t2;
-
-    // Hold the trigger pin high for at least 10 us
+    digitalWrite(_trig_pin, LOW);
+    delayMicroseconds(5);
     digitalWrite(_trig_pin, HIGH);
     delayMicroseconds(10);
     digitalWrite(_trig_pin, LOW);
-
-    // Wait for pulse on echo pin
-    while ( digitalRead(_echo_pin) == 0 );
-
-    // Measure how long the echo pin was held high (pulse width)
-    // Note: the micros() counter will overflow after ~70 min
-    t1 = micros();
-    while ( digitalRead(_echo_pin) == 1 );
-    t2 = micros();
-
-    _pulse_width = t2 - t1;
+      
+    pinMode(_echo_pin, INPUT);
+    _pulse_width = pulseIn(_echo_pin, HIGH);
     _timestamp = millis();
+
+    dprint("SR04 - Measured distance ");
+    dprint(_pulse_width > MAX_DIST ? 400 : CONVERSION_CONSTANT_CM * _pulse_width);
+    dprintln(" cm.");
 }
 
 long SR04::microsecondsToCentimeters(void)
 {
     return (long)(_pulse_width > MAX_DIST ? 400 : CONVERSION_CONSTANT_CM * _pulse_width);
 }
-
 
 long SR04::microsecondsToInches(void) 
 {
